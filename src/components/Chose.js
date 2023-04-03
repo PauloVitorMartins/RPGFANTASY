d4adfc16d5131aa20a825ea67819cd5dacc8c1e4
PauloVitorMartins <pm885144@gmail.com>
2023-04-03 14:33:37 -0300

    elderScrolls

--- src/components/Chose.js
+++ src/components/Chose.js
@@ -2,6 +2,8 @@ import '../css/header.css';
 
 export default function Chose () {
     return (
+        <div className='backgroundT'>
             <a className='chose'>Chose your Race</a>
+        </div>
     )
 }
